bb7d15b341127c153e82c5ca25383b61e765206f
obijzbo <nahiyanmubashshir@gmail.com>
2025-07-29 21:47:34 +0600

    new endpoint added

--- app/main.py
+++ app/main.py
@@ -7,4 +7,8 @@ app = FastAPI()
 def root():
     return {"message": "Hello from Lambda Function URL"}
 
+@app.get("/new")
+def new():
+    return {"message": "New Endpoint added in Lambda Function URL"}
+
 handler = Mangum(app)
